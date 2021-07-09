61906a0b6b8d2a16fb38854d2eef6a30aee46179
vodnicearleonid <vodnicear@gmail.com>
2021-07-09 12:59:57 +0300

    error

--- error.php
+++ error.php
@@ -5,15 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>php Blog</title>
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <link rel="icon" href="/img/favicon.ico">
+    <title>error</title>
 </head>
 <body>
-
+<h3> Error 404, The page you accessed does not exist, <a href="index.php">go to the main page</a> .</h3>
 </body>
 </html>
-
-<?php
-$string = "text";
-echo $string;
